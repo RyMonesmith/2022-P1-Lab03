4319ba15c2ac32a0ce8c83dd918a2914c94fe4f9
Ryan monesmith <ryan.monesmith@vtsd.com>
2022-09-21 08:17:39 -0400

    DOne

--- Lab03vst.java
+++ Lab03vst.java
@@ -2,13 +2,34 @@
 // Student starting version of the Lab03 assignment.
 // Resave this program as Lab03v80 for the 80 point version.
 // Resave this program as Lab03v100 for the 100 point version.
+/*
 
+
+
+
+ */
 
 public class Lab03vst
 {
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
+	  System.out.println("Starting seconds: 10000");
+	  int sec = 10000;
+
+
+	  int hours = sec/ 3600;
+	  System.out.println("hours: "+ hours);
+
+	  int minutes = (sec%3600)/60;
+	  System.out.println("minutes: "+ minutes);
+
+	  int seconds = (sec%3600)%60;
+	  System.out.println("seconds: "+ seconds);
+
+
+
+
       
 	}
 }
